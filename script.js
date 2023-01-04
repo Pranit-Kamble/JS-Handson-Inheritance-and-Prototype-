5e6e6b18326fe19e186b89678fef117a90b5febb
Pranit-Kamble <kpranit0134@gmail.com>
2023-01-04 16:27:21 +0530

    commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// let Parentobj={
+//     calAge:function(){
+//         console.log(`${this.fname} ${this.lname} ${this.age}`)
+//     }
+// }
+// const childobj=Object.create(Parentobj);
+// childobj.fname="Pranit"
+// childobj.lname="Kamble"
+// childobj.age="23"
+
+// childobj.calAge()
+
+
+// let Calsum={
+//     sumof:function(arr){
+//         let sum=0;
+//         for (const num of arr) {
+            
+//             sum=sum+num 
+//         }
+//         console.log(sum) 
+//     }
+// }
+// Calsum.sumof([1,2,3,4])
+// Calsum.sumof([2,4,5,7])
+// Calsum.sumof([3,2,1,6])
+
+
+
+
+
+// let obj={
+//     name:"Pranit",
+//     id:"7",
+//     Batch:"EA18"
+// }
+
+// for(var [k,v] of Object.entries(obj)) {
+//   console.log(k)
+// }
